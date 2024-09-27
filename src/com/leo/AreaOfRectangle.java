abbf8ee39de9f7fcd619fe98fde39f76590f88c2
leo <leomutungi19@gmail.com>
2024-09-27 21:59:54 +0300

    Area of rectangle

--- src/com/leo/AreaOfRectangle.java
+++ src/com/leo/AreaOfRectangle.java
@@ -1,7 +1,23 @@
 package com.leo;
 
-public class AreaOfRectangle {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+import static java.lang.System.in;
+
+public class AreaOfRectangle {
+    public static void main(String[] args) { //static means that the method belongs to the class not an
+        Scanner in = new Scanner(System.in);
+
+        System.out.println("Enter the length of the rectangle: "); //user prompt to enter the length of the rectangle
+        double length = in.nextDouble(); //reads any double input of the length
+
+        System.out.println("Enter width of the rectangle: "); // user inputs the width of the rectangle
+        double width = in.nextDouble();
+
+        // Area = L * W
+        double area = length * width;
+        System.out.println("The area of the rectangle is: " + area);
+
+        in.close();
     }
 }
